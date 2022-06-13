647f727665072f01c44b78adaf08f97eb91bbd09
yuanhao <71644411+Yuanhao-JIANG@users.noreply.github.com>
2022-06-13 17:32:53 +0100

    convert raw data to dataframe, and save to csv file

--- generate_data.py
+++ generate_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
 
@@ -10,24 +11,30 @@ feature_size = 15
 
 def generate_data():
     feature_data = np.zeros((feature_size, data_size))
+    cols = []
 
     # some generic feature:
     # gender
     feature_data[0] = np.random.binomial(1, .5, data_size)
+    cols.append('gender')
     # age
     age_mean, age_var = 28, 25
     feature_data[1] = stats.truncnorm.rvs((18-age_mean)/age_var, (80-age_mean)/age_var,
                                           loc=age_mean, scale=age_var, size=data_size)
+    cols.append('age')
     # car cost
     car_cost_mean, car_cost_var = 39000, 50000
     feature_data[2] = stats.truncnorm.rvs((10000-car_cost_mean)/car_cost_var, (400000-car_cost_mean)/car_cost_var,
                                           loc=car_cost_mean, scale=car_cost_var, size=data_size)
+    cols.append('car_cost')
     # miles
     miles_mean, miles_var = 8000, 50000
     feature_data[3] = stats.truncnorm.rvs((200-miles_mean)/miles_var, (250000-miles_mean)/miles_var,
                                           loc=miles_mean, scale=miles_var, size=data_size)
+    cols.append('miles')
     # brand
     feature_data[4] = np.random.uniform(0, 100, data_size)
+    cols.append('brand')
     # some random feature:
     feature_data[5] = np.random.binomial(10, .7, data_size)
     feature_data[6] = np.random.uniform(50, 100, data_size)
@@ -39,6 +46,8 @@ def generate_data():
     feature_data[12] = np.random.normal(1.5, 5, data_size)
     feature_data[13] = np.random.normal(1, 2, data_size)
     feature_data[14] = np.random.normal(2, 4, data_size)
+    for i in range(10):
+        cols.append(f'rand_feature_{i}')
 
     feature_data = np.transpose(feature_data)
 
@@ -68,10 +77,17 @@ def generate_data():
         # response[i] = t
         # response[i] = np.tanh(t/5.5+0.5)
         response[i] = np.random.binomial(1, (np.tanh(t/5.5+0.5) + 1)/2)
-        data = [[feature_data[i], price[i], response[i]] for i in range(data_size)]
-        data = np.array(data, dtype=object)
-        return data
+
+    price = np.array([price])
+    response = np.array([response])
+    cols.append('price')
+    cols.append('response')
+
+    data = np.concatenate((feature_data, price.T, response.T), axis=1)
+    return data, cols
 
 
-raw_data = generate_data()
+raw_data, cols = generate_data()
+df = pd.DataFrame(raw_data, columns=cols)
 np.save('./data/raw_data.npy', raw_data)
+df.to_csv('./data/dataframe.csv')
