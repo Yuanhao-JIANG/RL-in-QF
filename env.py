f9c0a9b26fe2de973e4fb53c5afbf37de353e97c
Yuanhao-JIANG <71644411+Yuanhao-JIANG@users.noreply.github.com>
2022-06-16 23:14:14 +0100

    wrap with class

--- env.py
+++ env.py
@@ -4,6 +4,48 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
 
+class Env:
+    def __init__(self, glm, df):
+        self.glm = glm
+
+        add_group(df)
+        self.df = df
+
+    def reset(self):
+        return count_group(self.df)
+
+    def step(self, c):
+        """
+        promote with a price
+        :param c: promoted price
+        :return: reward, the (grouped) dataframe after promotion
+        """
+
+        # add price to dataframe
+        self.df['price'] = c
+
+        # glm generated response -> 1: buyer, 0: won't buy
+        pred = self.glm.predict(self.df).gt(0.5).astype(int)  # <class 'pandas.core.series.Series'>
+
+        # add glm generated response to dataframe
+        self.df['response'] = pred
+
+        # total profit
+        r1 = (pred * (c - (self.df['car_cost'] / 300000 + self.df['rand_feature_0'] / 10) * c)).sum()
+
+        # group buyers percentage, there are 4 groups
+        k = 4
+        p = np.zeros(k)
+        for i in range(k):
+            p[i] = self.df.groupby(['group', 'response']).size()[i][1] / self.df.groupby(['group']).size()[i]
+
+        # variance of group buyers percentage, we want balanced buyers count for different groups
+        r2 = np.var(p, ddof=1)
+
+        # currently keep df unchanged
+        return np.max((0.6 - np.sqrt(r2)) * r1, 0), count_group(self.df)
+
+
 def add_group(df):
     """
     add a group column to the dataframe
@@ -15,52 +57,20 @@ def add_group(df):
     :return: None
     """
 
-    df['group'] = (df['age'] // 40)*2 + df['car_cost'].__gt__(70000).astype(int)
+    df['group'] = (df['age'] // 40) * 2 + df['car_cost'].__gt__(70000).astype(int)
+
+
+def count_group(df):
+    t = df.groupby(['group']).size()
+    return (t/t.sum()).values
 
 
 def add_observations(df_obs, df):
     """
-    add an observation to a grouped dataframe
-    :param df_obs: dataframe of observations
-    :param df: grouped dataframe
-    :return: the concatenated dataframe
-    """
+        add an observation to a grouped dataframe
+        :param df_obs: dataframe of observations
+        :param df: grouped dataframe
+        :return: the concatenated dataframe
+        """
     add_group(df_obs)
     return pd.concat([df, df_obs], ignore_index=True)
-
-
-def update_df(df, c):
-    return df
-
-
-def promote(df, c, glm):
-    """
-    promote with a price
-    :param df: current (grouped) dataframe (state)
-    :param c: promoted price
-    :param glm: the environment (fitted glm)
-    :return: reward, the (grouped) dataframe after promotion
-    """
-
-    # add price to dataframe
-    df['price'] = c
-
-    # glm generated response -> 1: buyer, 0: won't buy
-    pred = glm.predict(df).gt(0.5).astype(int)  # <class 'pandas.core.series.Series'>
-
-    # add glm generated response to dataframe
-    df['response'] = pred
-
-    # total profit
-    r1 = (pred*(c - (df['car_cost']/300000 + df['rand_feature_0']/10)*c)).sum()
-
-    # group buyers percentage, there are 4 groups
-    k = 4
-    p = np.zeros(k)
-    for i in range(k):
-        p[i] = df.groupby(['group', 'response']).size()[i][1] / df.groupby(['group']).size()[i]
-
-    # variance of group buyers percentage, we want balanced buyers count for different groups
-    r2 = np.var(p, ddof=1)
-
-    return np.max((0.6 - np.sqrt(r2))*r1, 0), update_df(df, c)
