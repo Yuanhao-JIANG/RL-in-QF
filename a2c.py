aeebd4a4bb040196a5a5c0583825399a8a58c1c5
Yuanhao-JIANG <71644411+Yuanhao-JIANG@users.noreply.github.com>
2022-06-17 15:11:22 +0100

    finish implementing a2c model

--- a2c.py
+++ a2c.py
@@ -40,7 +40,7 @@ class ActorCritic(nn.Module):
         )
 
     def forward(self, state):
-        state = Variable(torch.from_numpy(state).float().unsqueeze(0))  # [a,b,c,d] -> [[a,b,c,d]]
+        state = Variable(state.float().unsqueeze(0))  # [a,b,c,d] -> [[a,b,c,d]]
 
         value = self.critic_net(state)
 
@@ -50,19 +50,24 @@ class ActorCritic(nn.Module):
         return value, policy_dist
 
 
+# train method
 def a2c(environment):
     learning_rate = 3e-4
     gamma = 0.99
-    num_steps = 10
-    max_episodes = 3000
+    num_steps = 50
+    max_episodes = 2000
     num_state_features = 4
     price_low = 400
     price_high = 2700
     num_actions = price_high - price_low + 1
 
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
     actor_critic = ActorCritic(num_state_features, num_actions)
     ac_optimizer = optim.Adam(actor_critic.parameters(), lr=learning_rate)
 
+    actor_critic = actor_critic.to(device)
+    actor_critic.train()
     for episode in range(max_episodes):
         log_probs = []
         values = []
@@ -71,34 +76,36 @@ def a2c(environment):
         # go along a trajectory for num_steps steps
         state = environment.reset()
         for steps in range(num_steps):
-            # we are using q values to approximate value
+            state = state.to(device)
             value, policy_distro = actor_critic.forward(state)
+            value, policy_distro = value.to(device), policy_distro.to(device)
             value = value[0][0]
 
-            action = np.random.choice(num_actions, p=policy_distro.detach()[0].numpy())  # 0 or 1
+            action = np.random.choice(num_actions, p=policy_distro.detach().cpu()[0].numpy())  # 0 or 1
             c = action + price_low
             log_prob = torch.log(policy_distro.squeeze(0)[action])    # log( P(action) )
 
             # go to next state
             reward, new_state = environment.step(c)
+            new_state = new_state.to(device)
             rewards.append(reward)
             values.append(value)
             log_probs.append(log_prob)
             state = new_state
 
-            # record the q value for the last state
-            q_val, _ = actor_critic.forward(new_state)
-            q_val = q_val[0][0]
+            if episode % 10 == 0 and steps == num_steps - 1:
+                # record the value for the last state to compute the last q value
+                val_last, _ = actor_critic.forward(new_state)
+                val_last = val_last[0][0]
 
-        if episode % 10 == 0:
-            sys.stdout.write("episode: {}, reward: {} \n".format(episode, np.sum(rewards)))
+                sys.stdout.write("episode: {}, reward: {}, state: {}\n".format(episode, np.mean(rewards),
+                                                                               state.detach().cpu().numpy()))
 
         # compute Q values
         q_vals = values.copy()
-        for t in reversed(range(len(rewards))):
-            # at start, use the last q_value to approximate the last value
-            q_val = rewards[t] + gamma * q_val
-            q_vals[t] = q_val
+        q_vals[-1] = rewards[-1] + gamma * val_last
+        for t in reversed(range(len(rewards) - 1)):
+            q_vals[t] = rewards[t] + gamma * values[t + 1]
 
         # update actor critic
         values = torch.stack(values)
@@ -107,15 +114,13 @@ def a2c(environment):
 
         # using q values to approximate values
         advantage = q_vals - values
-        critic_loss = 0.5 * advantage.pow(2).mean()
-        actor_loss = (-log_probs * advantage).mean()
+        critic_loss = 0.5 * (values - q_vals.detach()).pow(2).mean()
+        actor_loss = (-advantage.detach() * log_probs).mean()
         ac_loss = actor_loss + critic_loss
 
         ac_optimizer.zero_grad()
         ac_loss.backward()
         ac_optimizer.step()
-
-        exit()
 
 
 df_train = pd.read_csv('./data/dataframe_train.csv')
