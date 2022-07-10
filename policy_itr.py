178d33beeaee04d8f098436e9c38e510153627e0
Yuanhao-JIANG <71644411+Yuanhao-JIANG@users.noreply.github.com>
2022-07-10 21:57:36 +0100

    Update model

--- policy_itr.py
+++ policy_itr.py
@@ -6,6 +6,15 @@ import torch.optim as optim
 from torch.autograd import Variable
 import statsmodels.api as sm
 import env
+import matplotlib.pyplot as plt
+
+
+class LogLayer(nn.Module):
+    def __init__(self):
+        super(LogLayer, self).__init__()
+
+    def forward(self, t):
+        return torch.log(t+torch.abs(torch.min(t)).detach()+1)
 
 
 class Net(nn.Module):
@@ -15,13 +24,11 @@ class Net(nn.Module):
 
         # value
         self.value_net = nn.Sequential(
-            nn.Linear(num_state_features, 128),
+            nn.Linear(num_state_features, 64),
             nn.ReLU(),
-            nn.Linear(128, 256),
+            nn.Linear(64, 128),
             nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 1),
+            nn.Linear(128, 1)
         )
 
         # policy
@@ -30,9 +37,10 @@ class Net(nn.Module):
             nn.ReLU(),
             nn.Linear(128, 256),
             nn.ReLU(),
-            nn.Linear(256, 512),
+            nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Linear(512, num_actions),
+            nn.Linear(128, num_actions),
+            LogLayer(),     # to make numbers not differ too much
             nn.Softmax(dim=0)
         )
 
@@ -50,8 +58,8 @@ class Net(nn.Module):
 
 # train method
 def policy_itr(environment):
-    np.random.seed(123)
-    torch.manual_seed(211)
+    # np.random.seed(123)
+    # torch.manual_seed(211)
 
     learning_rate = 3e-2
     gamma = 0.99
@@ -60,7 +68,8 @@ def policy_itr(environment):
     num_state_features = 18
     price_low = 400
     price_high = 2700
-    num_actions = price_high - price_low + 1
+    price_step = 20
+    num_actions = int((price_high - price_low)/price_step)
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -70,6 +79,8 @@ def policy_itr(environment):
     net = net.to(device)
     net.train()
 
+    plt.ion()
+
     for episode in range(max_episodes):
         state = torch.from_numpy(environment.reset()).to(device)
         total_reward = 0
@@ -78,11 +89,13 @@ def policy_itr(environment):
             # policy evaluation
             value, policy_distro, _ = net.forward(state)
             value, policy_distro = value.to(device)[0], policy_distro.to(device)
-            c = torch.multinomial(policy_distro.cpu(), 1, replacement=True)[0] + price_low
+            c = torch.multinomial(policy_distro.cpu(), 1, replacement=True)[0] * price_step + price_low
+            # print(policy_distro)
+            # exit()
 
             target = torch.tensor(0.).to(device)
             for i in range(len(policy_distro)):
-                if policy_distro[i] > 0.02:
+                if policy_distro[i] > 1.6e-2:
                     r, s_expect = environment.peek_expected(i + price_low)
                     _, _, v = net.forward(torch.from_numpy(s_expect).to(device))
                     target += policy_distro[i].detach() * (r + gamma * v[0])
@@ -93,7 +106,7 @@ def policy_itr(environment):
                 # policy improvement
                 q = []
                 for i in range(len(policy_distro)):
-                    if policy_distro[i] > 0.02:
+                    if policy_distro[i] > 1.6e-2:
                         r, s_expect = environment.peek_expected(i + price_low)
                         _, _, v = net.forward(torch.from_numpy(s_expect).to(device))
                         q.append(r + gamma * v[0].cpu())
@@ -109,8 +122,10 @@ def policy_itr(environment):
             state = torch.from_numpy(state).to(device)
             total_reward += reward
 
-        if episode % 10 == 0:
-            sys.stdout.write("episode: {}, avg_reward: {}\n".format(episode, total_reward/num_steps))
+        if episode % 5 == 0:
+            sys.stdout.write("Episode: {}, avg_reward: {}\n".format(episode, total_reward/num_steps))
+            plt.plot(episode, total_reward / num_steps, 'o')
+            plt.pause(0.25)
 
     torch.save(net.state_dict(), './data/policy_itr__model.pth')
 
