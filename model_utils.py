f2a3ddf54f67fcc857c0676933aae02ca39cf53d
Yuanhao-JIANG <71644411+Yuanhao-JIANG@users.noreply.github.com>
2022-07-18 10:54:15 +0100

    Iadd PPO model

--- model_utils.py
+++ model_utils.py
@@ -75,3 +75,12 @@ class Reinforce(nn.Module):
         policy_distro = self.net(state)
 
         return policy_distro
+
+
+class PPO(nn.Module):
+    def __init__(self, num_state_features, num_actions):
+        super(PPO, self).__init__()
+        self.net = None
+
+    def forward(self, state):
+        pass
