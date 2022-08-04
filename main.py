5b7a0fb839caefb14292d53eda972a7796aa8db1
Yuanhao-JIANG <71644411+Yuanhao-JIANG@users.noreply.github.com>
2022-08-04 15:18:01 +0100

    update

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+from data_utils import generate_dataframe, fit_glm, plot_csv
+from a2c import train_a2c
+from reinforce import train_reinforce
+from ppo import train_ppo
 
+# generate data and use that to fit glm
+# df = generate_dataframe(data_size=10000, save=True, path='./data/dataframe_fit.csv', seed=0)
+# glm = fit_glm(save=True)
+# print(glm.summary())
 
-df = [None, None, None]
-df[0] = pd.read_csv('data/a2c_out.csv', header=None)
-df[1] = pd.read_csv('data/reinforce_out.csv', header=None)
-df[2] = pd.read_csv('data/ppo_out.csv', header=None)
+# train different models
+# train_a2c()
+# train_reinforce()
+# train_ppo()
 
-fig, ax = plt.subplots(3)
-fig.set_size_inches(11, 13)
-fig.text(0.04, 0.5, 'moving average reward', va='center', rotation='vertical')
-fig.text(0.5, 0.06, 'iteration', ha='center')
-for idx in range(3):
-    if idx == 0:
-        ax[idx].set_title("moving average reward with a2c")
-    elif idx == 1:
-        ax[idx].set_title("moving average reward with reinforce")
-    else:
-        ax[idx].set_title("moving average reward with ppo")
-    y = np.zeros((df[idx].shape[0], df[idx].shape[1]))
-    x = np.arange(df[idx].shape[1])
-    for i in range(6):
-        y[i] = df[idx].to_numpy()[i]
-        ax[idx].plot(x, y[i])
-plt.show()
+# plot the result
+# plot_csv(models=['a2c', 'reinforce', 'ppo'], indices=[0, 0, 0])
